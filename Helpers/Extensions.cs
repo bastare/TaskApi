24464c37b50dc14bb6c287e3588bcf72682c7c3b
bastare <noobfrizz1@gmail.com>
2020-04-22 22:39:05 +0300

    test

--- Helpers/Extensions.cs
+++ Helpers/Extensions.cs
@@ -6,11 +6,6 @@ namespace Task.Helpers
 {
     public static class Extensions
     {
-        /// <summary>
-        /// Get name of current controller
-        /// </summary>
-        /// <param name="controller"></param>
-        /// <returns>name of controller</returns>
         public static string Controller(this ControllerBase controller) =>
             controller.ControllerContext.RouteData.Values["controller"]?.ToString()
                 ?? throw new ArgumentNullException(nameof(controller), "RouteData hasn`t value");
